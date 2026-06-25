1030496135d9b3e7e9f8f0146e21f0de63fc70c4
mirelasc23 <mirelamscomach@gmail.com>
2026-06-25 19:22:38 -0300

    aplica imagem de fundo na tela_lista

--- lib/view/tela_lista.dart
+++ lib/view/tela_lista.dart
@@ -32,24 +32,41 @@ class _TelaListaState extends State<TelaLista> {
 
   @override
   Widget build(BuildContext context) {
+    final tamanhoTela = MediaQuery.of(context).size;
+    
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.titulo),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          FloatingActionButton(
-            onPressed: () {
-              //Navigator.pushNamed(context, Rotas.telaForm);
-              print('BOTAO PRESSIONADO');
-            },
-            tooltip: 'adicionar usuario',
-            child: const Icon(Icons.add_box_rounded),
+      body: Container(
+        width: tamanhoTela.width,
+        height: tamanhoTela.height,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/login-fundo.png'), // Imagem Local
+            // image: NetworkImage('https://link-da-sua-imagem.com'), // Imagem da Internet
+            fit: BoxFit.cover, // Garante que a imagem cubra toda a tela
           ),
-        ],
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                FloatingActionButton(
+                  onPressed: () {
+                    //Navigator.pushNamed(context, Rotas.telaForm);
+                    print('BOTAO PRESSIONADO');
+                  },
+                  tooltip: 'adicionar usuario',
+                  child: const Icon(Icons.add_box_rounded),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       bottomNavigationBar: Navbar(
         paginaSelecionada: paginaSelecionada,
