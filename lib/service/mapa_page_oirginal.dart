6245508f9d328149cfa872c456bb1fda67a509f4
mirelasc23 <mirelamsccomach@gmail.com>
2026-07-08 12:41:58 -0300

    insere botao para lista de plantas com FloatingActionButton, sob um Stack

--- lib/service/mapa_page_oirginal.dart
+++ lib/service/mapa_page_oirginal.dart
@@ -51,34 +51,51 @@ class _MapaPageState extends State<MapaPage> {
           ? const Center(
               child: CircularProgressIndicator(),
             ) // Mostra um carregando enquanto o banco não responde
-          : FlutterMap(
-              options: MapOptions(
-                initialCenter: LatLng(posicao.latitude, posicao.longitude),
-                initialZoom: 13.0,
-              ),
-              children: [
-                TileLayer(
-                  urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                  userAgentPackageName: 'brotinho.ddm.ifsc',
+          : Stack(
+            children: [
+              FlutterMap(
+                options: MapOptions(
+                  initialCenter: LatLng(posicao.latitude, posicao.longitude),
+                  initialZoom: 13.0,
                 ),
-                MarkerLayer(
-                  markers: provider.plantas.map((planta) {
-                    return Marker(
-                      point: LatLng(planta.lat, planta.long),
-                      child: GestureDetector(
-                        onTap: () =>
-                            Navigator.pushNamed(context, Rotas.telaPlanta),
-                        child: Icon(
-                          Icons.location_on,
-                          color: Colors.green,
-                          size: 40,
+                children: [
+                  TileLayer(
+                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                    userAgentPackageName: 'brotinho.ddm.ifsc',
+                  ),
+                  MarkerLayer(
+                    markers: provider.plantas.map((planta) {
+                      return Marker(
+                        point: LatLng(planta.lat, planta.long),
+                        child: GestureDetector(
+                          onTap: () =>
+                              Navigator.pushNamed(context, Rotas.telaPlanta),
+                          child: Icon(
+                            Icons.location_on,
+                            color: Colors.red,
+                            size: 40,
+                          ),
                         ),
-                      ),
-                    );
-                  }).toList(),
-                ),
-              ],
+                      );
+                    }).toList(),
+                  ),
+                ],
+              ),
+              Positioned(
+              bottom: 16,
+              right: 16,
+              child: FloatingActionButton(
+                backgroundColor: Colors.orangeAccent,
+                onPressed: () {
+                  // Ação do seu botão aqui (ex: voltar para home, centralizar mapa, etc)
+                  print("Botão do mapa pressionado!");
+                  Navigator.pushNamed(context, Rotas.telaLista);
+                },
+                child: const Icon(Icons.list, color: Colors.white),
+              ),
             ),
+            ]
+          ),
     );
   }
 }
